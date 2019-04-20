c4078e61c41008e6e8071ec875c1489ae965ce9b
Dennis Probst <dennis.probst@gmail.com>
2019-04-20 12:17:16 +1000

    hello world

---
+++ src/Components/Map/Map.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Map extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+            Hello World
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Map
